7d4038729add4b3ae609aa523dfc302b9977c2b0
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-02-04 03:56:06 +0530

    Add project files.

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlackCoderBoxOfficeMovieApp.Models
+{
+    public class BlackCoderBoxOfficeMovieContext : DbContext
+    {
+        public BlackCoderBoxOfficeMovieContext (DbContextOptions<BlackCoderBoxOfficeMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlackCoderBoxOfficeMovieApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackCoderBoxOfficeMovieApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BlackCoderBoxOfficeMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<BlackCoderBoxOfficeMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "MovieName1",
+                        ReleaseDate = DateTime.Parse("2015-03-05"),
+                        Genre = "Romantic Comedy",
+                        Price = 7
+                    }
+                    
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
